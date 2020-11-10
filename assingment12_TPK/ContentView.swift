15180487d6a994140e96c19befb5790e55385295
tpk73 <tpaaka73@gmail.com>
2020-11-10 08:32:34 -0600

    Initial Commit

---
+++ assingment12_TPK/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  assingment12_TPK
+//
+//  Created by Tanner Parker on 11/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ assingment12_TPK/assingment12_TPKApp.swift
@@ -0,0 +1,17 @@
+//
+//  assingment12_TPKApp.swift
+//  assingment12_TPK
+//
+//  Created by Tanner Parker on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct assingment12_TPKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
